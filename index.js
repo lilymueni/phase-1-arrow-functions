9e141513591ee8ccc2e3d90f6736341aff9e9129
lilianmueninzioki <liliannzioki@gmail.com>
2024-03-20 12:50:50 +0300

    Arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(parameter1,parameter2) {
+    return ( parameter1/parameter2);
+}
+console.log(divide (2000/100));
+
+const square = (parameter) => {
+    return parameter*parameter
+};
+
+const add =(parameter1,parameter2) => {
+    return parameter1 +parameter2;
+};
+console.log(add(2+5));
